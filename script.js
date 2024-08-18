a0aaacb62174aa3abddbdbc0b1f50d7915faea2e
Vu Huy Sang <vuhuysang147@gmail.com>
2024-08-18 16:37:53 +0700

    update myQcReader

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
+let html5QrCode = new Html5Qrcode("myQrReader");
+
+
 async function openScanCamera() {
-    const display = util.getElements("#myQrReader")
-    const hide = util.getElements("#showQrReader")
-    hide.style.display = "none"
-    display.style.display = "block"
-    const html5QrCode = new Html5Qrcode("myQrReader");
+    util.replaceClassElement(".frameQrReader", "d-none", "d-flex", "one");
+
     const qrCodeSuccessCallback = (decodedText, decodedResult) => {
         /* handle success */
         if (confirm("Do you want to go to this link: " + decodedText + "?")) {
@@ -19,13 +19,16 @@ async function openScanCamera() {
 
 
 async function stopScanning() {
-    const html5QrCode = new Html5Qrcode("myQrReader");
+    // html5QrCode.stop({ facingMode: "environment" }, config, handleStopReader)
     html5QrCode.stop().then((ignore) => {
         // QR Code scanning is stopped.
-        const display = util.getElements("#myQrReader")
+        const display = util.getElements(".frameQrReader")
         display.style.display = "none"
         const hide = util.getElements("#showQrReader")
         hide.style.display = "block"
+
+
+        util.replaceClassElement(".frameQrReader", "d-block", "d-none", "one");
     }).catch((err) => {
         // Stop failed, handle it.
     });
